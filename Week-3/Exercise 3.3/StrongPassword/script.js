5e873ffce0a54205af3731d2d8d2bc3c039916bc
sreekarteegala <teegalasreekar@gmail.com>
2023-08-26 15:24:38 +0530

    Added form for validating strong password

--- Week-3/Exercise 3.3/StrongPassword/script.js
+++ Week-3/Exercise 3.3/StrongPassword/script.js
@@ -1,3 +1,9 @@
+const commonPwds = [
+    "password",
+    "12345678",
+    "87654321"
+]
+
 function isStrongPassword(pwd) {
     if (pwd.length < 8) {
         return "false - Too Short"
@@ -7,14 +13,24 @@ function isStrongPassword(pwd) {
         return "false - Contains 'password'";
     }
 
+    if(commonPwds.includes(pwd)){
+        return "false - Most commonly used Password";
+    }
+
     if (!/[A-Z]/.test(pwd)) {
         return "Doesnot contain capital Letter";
     }
 
-    return true;
+    return "Strong Password";
 };
 
-console.log(isStrongPassword("qwerty"));;
-console.log(isStrongPassword("password@12345"));
-console.log(isStrongPassword("qwerty123"));;
-console.log(isStrongPassword("Qwerty123"));
+function checkPwd(event){
+    event.preventDefault();
+
+    let pwdInput = document.getElementById('password').value;
+    let pwdStrength = isStrongPassword(pwdInput);
+
+    document.getElementsByTagName("p")[0].innerHTML = pwdStrength;
+}
+
+document.getElementById("button").addEventListener("click", checkPwd);
