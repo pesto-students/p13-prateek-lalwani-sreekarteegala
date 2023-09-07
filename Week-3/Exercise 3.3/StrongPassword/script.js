745f9213a3dd599b76c3986e7972e5221d223dc3
sreekarteegala <teegalasreekar@gmail.com>
2023-09-07 14:13:25 +0530

    Added a form to validate strong password

--- Week-3/Exercise 3.3/StrongPassword/script.js
+++ Week-3/Exercise 3.3/StrongPassword/script.js
@@ -1,20 +1,37 @@
+const pwd = document.getElementById('pwd')
+const confirmPwd = document.getElementById('confirmPwd')
+const errorNode = document.getElementById('error');
+
 function isStrongPassword(pwd) {
     if (pwd.length < 8) {
-        return "false - Too Short"
+        return "Password Too Short"
     }
 
     if (pwd.includes("password")) {
-        return "false - Contains 'password'";
+        return "Password Contains Text 'password'";
     }
 
     if (!/[A-Z]/.test(pwd)) {
-        return "Doesnot contain capital Letter";
+        return "Password Doesnot contain capital Letter";
     }
 
-    return true;
+    return "Password is Strong";
 };
 
-console.log(isStrongPassword("qwerty"));;
+document.getElementById('button').addEventListener('click', (e) => {
+    if (!pwd.value || !confirmPwd.value) {
+        errorNode.innerHTML = 'Please Enter Password';
+        errorNode.style.color = "red";
+    } else if (pwd.value !== confirmPwd.value) {
+        errorNode.innerHTML = 'Passwords Does not match';
+        errorNode.style.color = "red";
+    } else {
+        errorNode.innerHTML = isStrongPassword(pwd.value);
+        errorNode.style.color = "green";
+    }
+})
+
+console.log(isStrongPassword("qwerty"));
 console.log(isStrongPassword("password@12345"));
-console.log(isStrongPassword("qwerty123"));;
+console.log(isStrongPassword("qwerty123"));
 console.log(isStrongPassword("Qwerty123"));
